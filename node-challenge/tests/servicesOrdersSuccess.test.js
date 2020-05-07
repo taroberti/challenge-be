8a874d6d66a01c3855fec74d7d9e18fd1fb86099
Talita Roberti <talitaroberti@gmail.com>
2020-05-07 10:01:11 +0200

    Finally figured out how to test the services Orders and mock firebase.

--- node-challenge/tests/servicesOrdersSuccess.test.js
+++ node-challenge/tests/servicesOrdersSuccess.test.js
@@ -1,13 +1,8 @@
-//const { mockFirebase } = require('firestore-jest-mock');
-/*const {
-  mockCollection,
-  mockGet,
-} = require('firestore-jest-mock/mocks/firestore');
-*/
 const ordersServices = require('../services/orders');
+
 const order1 = {
   id: '123nmk765',
-  order: {
+  data: () => ({
     title: 'Test title',
     bookingDate: 0,
     address: {
@@ -21,12 +16,12 @@ const order1 = {
       email: 'test@test.com',
       phone: 12345
     }
-  }
+  })
 };
 
 const order2 = {
   id: '678vsr432',
-  order: {
+  data: () => ({
     title: 'Test title 2',
     bookingDate: 0,
     address: {
@@ -40,35 +35,70 @@ const order2 = {
       email: 'test2@test.com',
       phone: 76543
     }
-  }
+  })
 };
 
-const mockOrders = {orders: [order1, order2]};
-const mockOrder = order1;
-const mockPost = { id: '678vsr432' };
-const mockPut = { success: 200 };
+const orderById = {
+  exists: true,
+  id: '678vsr432',
+  data: () => ({
+    title: 'Test title 2',
+    bookingDate: 0,
+    address: {
+      street: 'Str 2',
+      zip: '1098',
+      city: 'Berlin',
+      country: 'Germany'
+    },
+    customer: {
+      name: 'Test 2',
+      email: 'test2@test.com',
+      phone: 76543
+    }
+  })
+};
 
-const data = { data: [order1, order2] };
-const mockSnapshot = { val: () => data, exportVal: () => data, exists: jest.fn(() => true) };
-/* Tried to use jest.mock here, but would need to mock the snapshot that firebase returned, not so easy.
+const mockDataOrders = [order1, order2];
+const mockDataOrder = orderById;
+const mockAdd = { id: '678vsr432' };
+const mockUpdate = { success: 200 }
+
 jest.mock('../services/firebase', () => {
-  //const set = jest.fn();
   return {
     collection: jest.fn(col => ({
-      get: jest.fn(() => Promise.resolve(mockSnapshot)),
+      get: jest.fn(() => Promise.resolve(mockDataOrders)),
+      add: jest.fn(() => Promise.resolve(mockAdd)),
+      doc: jest.fn(() => ({
+        get: jest.fn(() => Promise.resolve(mockDataOrder)),
+        update: jest.fn(() => Promise.resolve(mockUpdate))
+      }))
     })),
   };
 });
-*/
 
 test('getOrders should return the orders from the database', async () => {
-  /* Tried to use mockFirebase as well, but getting an error:
-  Cannot find module 'firebase' from 'node_modules/firestore-jest-mock/mocks/firebase.js'
-  mockFirebase({
-    database: {
-      orders: [order1, order2]
-    }
-  });*/
+  const res = await ordersServices.getOrders();
 
-  //const res = await ordersServices.getOrders();
+  res.forEach((order, index) => {
+    expect(order.id).toEqual(mockDataOrders[index].id);
+    expect(order.order).toEqual(mockDataOrders[index].data());
+  })
+});
+
+test('getOrderById should return the order from the database', async () => {
+  const res = await ordersServices.getOrderById(orderById.id);
+
+  expect(res).toEqual(mockDataOrder.data());
+});
+
+test('postOrder should update the database with the new order', async () => {
+  const res = await ordersServices.postOrder();
+
+  expect(res.id).toEqual(mockAdd.id);
+});
+
+test('putOrder should update the order in the database', async () => {
+  const res = await ordersServices.putOrder(mockAdd.id);
+
+  expect(res.success).toEqual(mockUpdate.success);
 });
